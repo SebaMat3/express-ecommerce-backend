11490f9635d6e466d8bd6f5c1e89c9c8be848a6b
Sebastian Carballo <sebastiancarballoes@gmail.com>
2024-12-31 15:07:54 -0300

    SSL fix on sequelize config, troubleshooting

--- api/libs/sequelize.js
+++ api/libs/sequelize.js
@@ -8,19 +8,42 @@ const { setupModels } = require('./../db/models');
 const options = {
   dialect: 'postgres',
   logging: config.isProd ? false : console.log,
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
 }
 
 if (config.isProd) {
   options.dialectOptions = {
 	  ssl : {
 		  require : true,
-      sslmode: 'require'
-    }
-  }
+      rejectUnauthorized: true
+    },
+    keepAlive: true,
+  };
+  options.ssl = true;
 }
+
+// Log the configuration (temporary for debugging)
+console.log('Database URL:', config.dbUrl);
+console.log('Is Production:', config.isProd);
+console.log('Connection Options:', JSON.stringify(options, null, 2));
 
 
 const sequelize = new Sequelize (config.dbUrl, options)
+
+// Test the connection
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Database connection successfully established.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 setupModels(sequelize);
 
